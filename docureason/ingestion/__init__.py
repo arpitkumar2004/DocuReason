120a40a5c38ab5044b13257accc982bd43ac88a0
arpitkumar2004 <arpitkumar@kgpian.iitkgp.ac.in>
2026-08-07 06:34:09 +0530

    fix: function calling

--- docureason/ingestion/__init__.py
+++ docureason/ingestion/__init__.py
@@ -1,5 +1,6 @@
 """Offline Ingestion & Layout Parsing Subsystem for DocuReason."""
 
 from docureason.pipeline import DocuReasonPipeline, Phase1Pipeline
+from src.tripath.ingestion.docling_wrapper import DoclingWrapper
 
-__all__ = ["DocuReasonPipeline", "Phase1Pipeline"]
+__all__ = ["DocuReasonPipeline", "Phase1Pipeline", "DoclingWrapper"]
